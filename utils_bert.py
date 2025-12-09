6fd90a46b89437dd3a6cd0e309044170f3052150
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2025-12-10 00:28:19 +0330

    +

--- utils_bert.py
+++ utils_bert.py
@@ -167,6 +167,7 @@ def test_ce(epoch, model, loader, device, criterion, best_acc, save_folder):
             loss = criterion(outputs, targets)
 
             test_loss += loss.item()
+            print(outputs)
             _, predicted = outputs.max(1)
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()

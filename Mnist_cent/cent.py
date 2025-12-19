65b624cdab847c7acf3de94284f064f7452bd3d0
Hamed Mohammadzadeh <hamedmuhammadzadeh1380@gmail.com>
2025-12-19 21:06:36 +0330

    +

--- Mnist_cent/cent.py
+++ Mnist_cent/cent.py
@@ -180,51 +180,99 @@ class CenterLoss(nn.Module):
         return loss
 
 
-class CenterLossNormal(nn.Module):
-    """Center loss.
+# class CenterLossNormal(nn.Module):
+#     """Center loss.
     
-    Reference:
-    Wen et al. A Discriminative Feature Learning Approach for Deep Face Recognition. ECCV 2016.
+#     Reference:
+#     Wen et al. A Discriminative Feature Learning Approach for Deep Face Recognition. ECCV 2016.
     
-    Args:
-        num_classes (int): number of classes.
-        feat_dim (int): feature dimension.
-    """
-    def __init__(self, num_classes=10, feat_dim=64, use_gpu=True, init_value = None, rad = 1.0):
-        super(CenterLossNormal, self).__init__()
-        self.num_classes = num_classes
-        self.feat_dim = feat_dim
-        self.use_gpu = use_gpu
-        self.init_value = init_value
+#     Args:
+#         num_classes (int): number of classes.
+#         feat_dim (int): feature dimension.
+#     """
+#     def __init__(self, num_classes=10, feat_dim=64, use_gpu=True, init_value = None, rad = 1.0):
+#         super(CenterLossNormal, self).__init__()
+#         self.num_classes = num_classes
+#         self.feat_dim = feat_dim
+#         self.use_gpu = use_gpu
+#         self.init_value = init_value
 
-        if self.use_gpu:
-            self.centers = nn.Parameter(torch.randn(self.num_classes, self.feat_dim).cuda()) * rad
-        else:
-            self.centers = nn.Parameter(torch.randn(self.num_classes, self.feat_dim))* rad
+#         if self.use_gpu:
+#             self.centers = nn.Parameter(torch.randn(self.num_classes, self.feat_dim).cuda()) * rad
+#         else:
+#             self.centers = nn.Parameter(torch.randn(self.num_classes, self.feat_dim))* rad
 
-        if self.init_value is not None:
+#         if self.init_value is not None:
 
-            with torch.no_grad():
-                self.centers.copy_(self.init_value)
+#             with torch.no_grad():
+#                 self.centers.copy_(self.init_value)
 
             
-    def forward(self, x, labels):
-        """
-        Args:
-            x: feature matrix with shape (batch_size, feat_dim).
-            labels: ground truth labels with shape (batch_size).
-        """
-        batch_size = x.size(0)
-        distmat = torch.pow(x, 2).sum(dim=1, keepdim=True).expand(batch_size, self.num_classes) + \
-                  torch.pow(self.centers, 2).sum(dim=1, keepdim=True).expand(self.num_classes, batch_size).t()
-        distmat.addmm_(1, -2, x, self.centers.t())
+#     def forward(self, x, labels):
+#         """
+#         Args:
+#             x: feature matrix with shape (batch_size, feat_dim).
+#             labels: ground truth labels with shape (batch_size).
+#         """
+#         batch_size = x.size(0)
+#         distmat = torch.pow(x, 2).sum(dim=1, keepdim=True).expand(batch_size, self.num_classes) + \
+#                   torch.pow(self.centers, 2).sum(dim=1, keepdim=True).expand(self.num_classes, batch_size).t()
+#         distmat.addmm_(1, -2, x, self.centers.t())
 
-        classes = torch.arange(self.num_classes).long()
-        if self.use_gpu: classes = classes.cuda()
-        labels = labels.unsqueeze(1).expand(batch_size, self.num_classes)
-        mask = labels.eq(classes.expand(batch_size, self.num_classes))
+#         classes = torch.arange(self.num_classes).long()
+#         if self.use_gpu: classes = classes.cuda()
+#         labels = labels.unsqueeze(1).expand(batch_size, self.num_classes)
+#         mask = labels.eq(classes.expand(batch_size, self.num_classes))
 
-        dist = distmat * mask.float()
-        loss = dist.clamp(min=1e-12, max=1e+12).sum() / batch_size
+#         dist = distmat * mask.float()
+#         loss = dist.clamp(min=1e-12, max=1e+12).sum() / batch_size
 
+#         return loss
+
+from torch.autograd.function import Function
+
+class CenterLossNormal(nn.Module):
+    def __init__(self, num_classes, feat_dim, size_average=True, init_value = None):
+        super(CenterLossNormal, self).__init__()
+        self.centers = nn.Parameter(torch.randn(num_classes, feat_dim))
+        if init_value is not None: 
+            with torch.no_grad():
+                self.centers.copy_(init_value)
+        
+        self.centerlossfunc = CenterlossFunc.apply
+        self.feat_dim = feat_dim
+        self.size_average = size_average
+
+    def forward(self, label, feat):
+        batch_size = feat.size(0)
+        feat = feat.view(batch_size, -1)
+        # To check the dim of centers and features
+        if feat.size(1) != self.feat_dim:
+            raise ValueError("Center's dim: {0} should be equal to input feature's \
+                            dim: {1}".format(self.feat_dim,feat.size(1)))
+        batch_size_tensor = feat.new_empty(1).fill_(batch_size if self.size_average else 1)
+        loss = self.centerlossfunc(feat, label, self.centers, batch_size_tensor)
         return loss
+
+
+class CenterlossFunc(Function):
+    @staticmethod
+    def forward(ctx, feature, label, centers, batch_size):
+        ctx.save_for_backward(feature, label, centers, batch_size)
+        centers_batch = centers.index_select(0, label.long())
+        return (feature - centers_batch).pow(2).sum() / 2.0 / batch_size
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        feature, label, centers, batch_size = ctx.saved_tensors
+        centers_batch = centers.index_select(0, label.long())
+        diff = centers_batch - feature
+        # init every iteration
+        counts = centers.new_ones(centers.size(0))
+        ones = centers.new_ones(label.size(0))
+        grad_centers = centers.new_zeros(centers.size())
+
+        counts = counts.scatter_add_(0, label.long(), ones)
+        grad_centers.scatter_add_(0, label.unsqueeze(1).expand(feature.size()).long(), diff)
+        grad_centers = grad_centers/counts.view(-1, 1)
+        return - grad_output * diff / batch_size, None, grad_centers / batch_size, None
